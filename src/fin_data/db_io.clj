ba078516dd3362bdddb95c9ff91b750425f14668
Todd Stout <todd.tstout@gmail.com>
2023-04-25 18:11:43 -0500

    update fn docs

--- src/fin_data/db_io.clj
+++ src/fin_data/db_io.clj
@@ -3,10 +3,11 @@
             [sys-loader.core :as sys]
             [next.jdbc :as jdbc]))
 
-
-(defn data-src []
-  (-> @sys/sys-state 
-      :sys/db 
+(defn data-src
+  "Grab the datasource from the sys-loader context"
+  []
+  (-> @sys/sys-state
+      :sys/db
       :data-source))
 
 (def sql-text
@@ -18,16 +19,23 @@
          io/resource
          slurp))))
 
-(defn csv-import-sql []
+(defn csv-import-sql
+  "Load the sql for csv import. This is a bit of a hack
+   as I could not get ? jdbc placeholder to work in this case.
+   The csv file to import is expected to be at ~/Downloads/stmt.csv"
+  []
   (-> (sql-text :csv-read)
-      (.replace "{{file-name}}" 
+      (.replace "{{file-name}}"
                 (str (System/getProperty "user.home") "/Downloads/stmt.csv"))))
 
-
-(defn import-csv []
+(defn import-csv
+  "Import BOA CSV file into import table
+   finkratzen.boa_import"
+  []
   (with-open [conn (jdbc/get-connection (data-src))]
-      (jdbc/execute-one! conn
-                     [(csv-import-sql)])))
+    (jdbc/execute-one! conn ["drop table if exists finkratzen.boa_import"])
+    (jdbc/execute-one! conn
+                       [(csv-import-sql)])))
 
 
 (comment
@@ -35,6 +43,8 @@
   (data-src)
   (import-csv)
   (csv-import-sql)
+
+  (time (import-csv))
 
   (System/getProperty "user.home")
 
