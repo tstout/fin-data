398f34ca6d57332ec0126b09aa7ca6dd1754985a
Todd Stout <todd.tstout@gmail.com>
2024-07-21 12:40:33 -0500

    add jmx remote connectivity to jvm args

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-clojure -M:fin-data server
+clojure -J-Dcom.sun.management.jmxremote \
+-J-Dcom.sun.management.jmxremote.port=8004 \
+-J-Dcom.sun.management.jmxremote.authenticate=false \
+-J-Dcom.sun.management.jmxremote.ssl=false \
+-J-Djava.rmi.server.hostname=stout-pi4.local \
+-M:fin-data server
